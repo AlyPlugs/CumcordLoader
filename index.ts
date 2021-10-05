4075177a368c873523e6745b8458f28ec34c597f
Alyxia Sother <lexisoth2005@gmail.com>
2021-10-05 16:36:40 +0200

    Add plugin

---
+++ index.ts
@@ -0,0 +1,72 @@
+/* eslint-disable no-invalid-this */
+import { UPlugin } from '@classes';
+
+export default class CumcordLoader extends UPlugin {
+  public appId = Astra.commands.registerSection({
+    name: 'Cumcord',
+    icon: 'https://avatars.githubusercontent.com/u/81397549?s=500'
+  });
+  public window = window as any;
+  start(): void {
+    fetch('https://raw.githubusercontent.com/Cumcord/Cumcord/stable/dist/build.js').then(res =>
+      res.text().then(text => {
+        eval(text);
+        Astra.commands.registerCommand('Cumcord', {
+          name: 'plugin',
+          description: 'Manage Cumcord plugins.',
+          aliases: [],
+          applicationId: this.appId,
+          execute: async options => {
+            if (options.add) {
+              let plugUrl = options.add[0].text;
+              if (
+                plugUrl.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g)
+              ) {
+                plugUrl = options.add[0].text.replace(/\/\s*$/, '');
+                const res = await fetch(`${plugUrl}/plugin.json`);
+                const manifest = await res.json();
+                console.log(manifest.name);
+                this.window.cumcord.plugins.importPlugin(plugUrl);
+                return {
+                  content: `Added \`${manifest.name}\``
+                };
+              }
+              return {
+                content: 'Invalid plugin URL!'
+              };
+            }
+
+            if (options.remove)
+              // this.window.cumcord.plugins.removePlugin(options.remove[0].text);
+              return {
+                content: `Removed \`${options.remove[0].text}\``
+              };
+          },
+          options: [
+            {
+              name: 'add',
+              description: 'The plugin to add.',
+              type: 3
+            },
+            {
+              name: 'remove',
+              description: 'The plugin to remove.',
+              type: 3,
+              choices: this.window.cumcord.plugins.pluginCache
+                ? Object.keys(this.window.cumcord.plugins.pluginCache).map(plugin => ({
+                  name: this.window.cumcord.plugins.pluginCache[plugin].manifest.name,
+                  value: plugin
+                }))
+                : []
+            }
+          ]
+        });
+      }));
+  }
+  stop(): void {
+    this.window.cumcord.uninject();
+    Astra.commands.unregisterSection(this.appId);
+    Astra.commands.unregisterCommands('hi');
+    Astra.n11s.show('Cumcord has unloaded.');
+  }
+}
